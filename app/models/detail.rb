c5cb59bcc91e7be585033bb8d345a4709804847f
ez <eric@Eric-Laquers-Computer.local>
2010-07-18 21:07:40 -0400

    Refactored GovernmentDetail stuff to Detail

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -5,4 +5,35 @@ class Detail < ActiveRecord::Base
   belongs_to :entity
   validates_presence_of :entity
   validates_associated :entity
+
+  def name
+    self.entity.name
+  end
+
+  def self.entities
+    Entity.find_by_type(self.to_s)
+  end
+
+  def self.add_entity_detail(attrs)
+    self.entity_class.add_entity_detail( self, attrs )
+  end
+
+  def self.find_by_name_and_details( name, details )
+    entity_details= self.named(name).find( :all, :conditions => details ).compact
+    #TODO error check
+    entity= if entity_details.length > 0 then entity_details[0].entity else nil end
+    return entity, entity_details #.map{|ed| ed.detail}
+  end
+
+  def self.find_or_add_name_details( name, unique_details={}, other_details={} )
+    rec= nil
+    entity, details =
+      self.find_by_name_and_details( name , unique_details )
+    if entity.nil?
+     entity, details =
+       self.add_entity_detail( { :name => name }.merge( unique_details).merge( other_details) )
+    end
+    return entity, details
+  end
+
 end
--- app/models/government/government_detail.rb
+++ app/models/government/government_detail.rb
@@ -1,17 +1,13 @@
 class Government::GovernmentDetail < Detail
 
+  def self.entity_class
+    Government::Government
+  end
+
   belongs_to :government, :class_name => 'Entity', :foreign_key => :entity_id
-  named_scope :named, lambda { |name| 
+  named_scope :named, lambda { |name|
     { :include => :government, :conditions => ["entities.name = ?", name] }
   }
-
-  def self.entities
-    Entity.find_by_type(self.to_s)
-  end
-
-  def name 
-    self.entity.name
-  end
 
   def self.purge
     Government::Country.delete_all
@@ -21,28 +17,6 @@ class Government::GovernmentDetail < Detail
     Government::Neighborhood.delete_all
     Government::SchoolDistrict.delete_all
     Government::EntityDetail.delete_all
-  end
-
-  def self.add_entity_detail(attrs)
-    Government::Government.add_entity_detail( self, attrs )
-  end
-
-  def self.find_by_name_and_details( name, details )
-    entity_details= self.named(name).find( :all, :conditions => details ).compact
-    #TODO error check
-    entity= if entity_details.length > 0 then entity_details[0].entity else nil end
-    return entity, entity_details #.map{|ed| ed.detail}
-  end
-
-  def self.find_or_add_name_details( name, unique_details={}, other_details={} )
-    rec= nil
-    entity, details =
-      self.find_by_name_and_details( name , unique_details )
-    if entity.nil?
-     entity, details =
-       self.add_entity_detail( { :name => name }.merge( unique_details).merge( other_details) )
-    end
-    return entity, details
   end
 
 end
