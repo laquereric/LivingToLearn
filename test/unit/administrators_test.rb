50d4f4a847a01026983d29f103a7ec4f965bb838
ez <eric@Eric-Laquers-Computer.local>
2010-08-19 13:15:35 -0400

    Added Administrators spreadsheet

---
+++ test/unit/administrators_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class AdminstratorsTest < ActiveSupport::TestCase
+  attr_accessor :filename
+
+  def setup
+  end
+
+  def test_ods_cell_values
+    self.filename= File.join( RAILS_ROOT, "test" , "fixtures" , "ADMINISTRATORS.ods")
+    confirm_cell_values
+  end
+
+  def confirm_cell_values
+    spreadsheet= Spreadsheet::Administrators.load_record_file( filename )
+    assert  Spreadsheet::Administrators.check_headers, true
+    Spreadsheet::Administrators.headers.each_index{ |index|
+      assert_equal spreadsheet.cell(1,index+1),Spreadsheet::Administrators.headers[index]
+    }
+  end
+
+end
