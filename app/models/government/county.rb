bdf2d3cc5fdb1416c831b8ce5f81d685ea4231e1
ez <eric@Eric-Laquers-Computer.local>
2010-09-04 13:51:50 -0400

    Print Formatting

--- app/models/government/county.rb
+++ app/models/government/county.rb
@@ -2,4 +2,13 @@ class Government::County < Government::GovernmentDetail
   set_table_name :government_counties
   belongs_to :state_entity, :class_name => "Entity", :foreign_key => :government_state_id
   belongs_to :country_entity, :class_name => "Entity", :foreign_key => :government_country_id
+
+  def self.full_name_pretty(entity)
+    "#{self.name_pretty(entity)} County"
+  end
+
+  def self.name_pretty(entity)
+    entity.name.split('_').map{ |n| n.capitalize}.join(' ')
+  end
+
 end
