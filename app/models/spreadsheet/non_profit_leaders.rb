0341ede65c65d539efa9f6ce89c07fc11286ba88
ez <eric@Eric-Laquers-Computer.local>
2010-09-07 17:19:51 -0400

    Cleanest Spreadsheet mapper yet (but still SOME class attributes)

---
+++ app/models/spreadsheet/non_profit_leaders.rb
@@ -0,0 +1,32 @@
+class Spreadsheet::NonProfitLeaders < Spreadsheet::Spreadsheet
+  attr_accessor :school_district
+
+  def initialize(school_district)
+    self.school_district= school_district
+    self.class.filename= self.google_path
+    self.class.load_record_hash_array
+  end
+
+  def google_path
+    File.join( self.school_district.google_foldername, google_filename )
+  end
+
+  def self.headers
+    [
+    'select','Prefix','FirstName','MiddleName','LastName','Suffix',
+    'Title','NonProfitName',
+    'AddressLine1',
+    'AddressLine2',
+    'City','State','Zip',
+    'Phone','Extension',
+    'Email','Web','Spoke'
+    ]
+  end
+
+  def self.clean_row_hash(row_hash)
+    row_hash[:zip]= "%.5i" % row_hash[:zip]
+    row_hash
+  end
+
+end
+
