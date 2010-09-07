f0595e1251368090b1034e4a4326da1cf4bf6c80
ez <eric@Eric-Laquers-Computer.local>
2010-09-07 16:43:24 -0400

    Removed noise from school district

--- app/models/government/school_district.rb
+++ app/models/government/school_district.rb
@@ -9,44 +9,10 @@ class Government::SchoolDistrict < Government::GovernmentDetail
     :class_name => "Person::SchoolDistrictAdministrator",
     :foreign_key => :government_school_district_detail_id
 
-  attr_accessor :spreadsheets
+#  has_many :organization_non_profit_details,
+#    :class_name => "Organization::NonProfit",
+#    :foreign_key => :government_school_district_detail_id
 
-  def self.csv_for_records(records)
-    p "Pulling CSV from database"
-    csv= Document::Csv.new
-    csv.record_hash_array= records.map{ |sd|
-      sd.get_csv_hash()
-    }
-    csv.set_header_symbols_from_hash
-    csv.set_header_titles_from_symbols
-    csv.get_output
-  end
-
-  def get_csv_hash()
-    rh = {}
-    rh[:country]= self.country_entity.name
-    rh[:state]= self.state_entity.name
-    rh[:county]= Government::County.full_name_pretty(self.county_entity)
-    rh[:school_district]= Government::SchoolDistrict.full_name_pretty(self.entity)
-    [
-        :district_code,
-        :at_risk_pupils_fy2011,
-        :poverty_pupils_fy2011,
-        :ses_allocation_fy2011,
-        :per_pupil_allocation_fy2011,
-        :after_school_hours,
-        :after_school_weeks,
-        :at_home_hours,
-        :at_home_weeks
-    ].each{|f|
-        rh[f]= self.send(f)
-    }
-    rh
-  end
-
-  def self.active_list
-    self.all.select{ |sd| sd.status == "active" }
-  end
 
   def self.full_name_pretty(entity)
     "#{self.name_pretty(entity)} School District"
@@ -56,50 +22,17 @@ class Government::SchoolDistrict < Government::GovernmentDetail
     entity.name.split('_').map{ |n| n.capitalize}.join(' ')
   end
 
-  def make_active
-    self.status="active"
-    r= save!
-  end
+#############
 
-  def spreadsheet_klass(key)
-    klass= Spreadsheet::Spreadsheet.spreadsheet_file_keys[key]
-    ss_klass= if klass then
-      klass
-    else
-      Spreadsheet::Spreadsheet
-    end
-    return ss_klass
-  end
-
-  def self.csv_filename(key)
-    File.join( RAILS_ROOT,'tmp','merge',"#{key.to_s}.csv" )
-  end
-
-  def csv_spreadsheet(key)
-    ss_klass= spreadsheet_klass(key)
-    ss_filename= spreadsheet_filename(key)
-    csv_filename= self.class.csv_filename(key)
-    ss_klass.csv_record_file(ss_filename,csv_filename)
-  end
-
-  def csv_spreadsheets
-    spreadsheets= {}
-    Spreadsheet::Spreadsheet.spreadsheet_file_keys.each_key{ |key|
-      csv_spreadsheet(key)
-    }
-  end
-
-  def self.google_foldername
+  def google_base_folder
     File.join( "TutoringClub" , "Data" , "Government_SchoolDistricts")
   end
 
   def google_foldername
-    File.join( self.class.google_foldername,"#{district_code}__#{name}")
+    File.join( self.google_base_folder,"#{self.district_code}__#{name}")
   end
 
-  def spreadsheet_filename(key)
-    File.join( google_foldername, "#{key.to_s.camelcase}.gxls")
-  end
+#########
 
   def self.at_cursor
     cursor_file= File.open(self.cursor_filename, 'r')
@@ -118,6 +51,8 @@ class Government::SchoolDistrict < Government::GovernmentDetail
       f.write(text)
     }
   end
+
+###########
 
   def history_total_funded_pupils_fy2010
     at_risk_pupils_fy2010 + poverty_pupils_fy2010
