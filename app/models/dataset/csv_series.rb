3d122b34b653676dfcf5cd3f7b2024c42ebdb2d0
ez <eric@Eric-Laquers-Computer.local>
2010-09-08 21:19:33 -0400

    Csv Series for many-up documents

--- app/models/dataset/csv_series.rb
+++ app/models/dataset/csv_series.rb
@@ -1,30 +1,52 @@
-class Dataset::Csv_3x < Dataset::Csv
+class Dataset::CsvSeries < Dataset::Csv
 
   attr_accessor :offset_records
 
   def prefix(offset,record_hash)
-    r={}
+    r= {}
     record_hash.each_pair{ | k, v |
       rk="r#{offset}_#{k.to_s}".to_sym
-      r[rk]=v
+      r[rk]= v
     }
     r
   end
 
+  def assemble_record()
+    r= {}
+    self.offset_records.each{ |h|
+      r.merge!(h)
+    }
+    r.merge(common_hash)
+    offset_records= []
+    r
+  end
+
   def record_merge(record_number,record_hash,common_hash)
-    offset_records=[] if offset_records.nil?
+    self.offset_records=[] if offset_records.nil?
     offset = record_number & 3
-    offset_records[seq]= prefix(offset,record_hash)
+    self.offset_records[offset]= prefix(offset,record_hash)
     r= case offset
       when 0 , 1: nil
-      when 2 : 
-        r= {}
-        offset_records.each{ |h|
-          r.merge!(h)
-        }
-        r.merge(common_hash)
-        offset_records= []
+      when 2 :
+        assemble_record()
     end
+  end
+
+  def fill_out_with(record_hash)
+    l= offset_records.length 
+    return if l == 0
+    return if  l == 3
+    for i in l..2 do
+      offset_records<< prefix( l, record_hash )
+    end
+    assemble_record()
+  end
+
+  def get_merged_hash()
+    super
+    self.merged_hash<< fill_out_with( record_hash_array[record_number-1] )
+    self.merged_hash.compact!
+    self.merged_hash
   end
 
 end
