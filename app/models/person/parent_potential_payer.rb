ef7521eb3854be56ac705581e7be89bf16c1bb45
ez <eric@Eric-Laquers-Computer.local>
2010-09-19 14:59:24 -0400

    Person::PatrentPotentialPayer

---
+++ app/models/person/parent_potential_payer.rb
@@ -0,0 +1,7 @@
+class Person::ParentPotentialPayer < Person::PersonDetail
+
+  set_table_name :person_parent_potnetial_payer
+  belongs_to :government_school_detail, :class_name => "Government::School", :foreign_key => :government_school_detail_id
+  belongs_to :government_school_district_detail, :class_name => "Government::SchoolDistrict", :foreign_key => :government_school_district_detail_id
+
+end
