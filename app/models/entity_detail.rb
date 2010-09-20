a05a3e6a62fd665546657a10083b1c7c9a66f49b
ez <eric@Eric-Laquers-Computer.local>
2010-09-20 12:06:38 -0400

    Whitespace

--- app/models/entity_detail.rb
+++ app/models/entity_detail.rb
@@ -6,7 +6,7 @@ class EntityDetail < ActiveRecord::Base
   named_scope :with_entity_id, lambda { |eid| {:conditions => ["entity_id = ?", eid ] } }
   named_scope :with_detail_type, lambda { |dt| {:conditions => ["detail_type = ?", dt ] } }
   named_scope :with_detail_id, lambda { |did| {:conditions => ["detail_id = ?", did ] } }
- 
+
   validates_presence_of :entity
   validates_associated :entity
 
