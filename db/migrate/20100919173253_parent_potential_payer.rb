a16460ef27c2db57b51ed9d6a90051cf9b28de04
ez <eric@Eric-Laquers-Computer.local>
2010-09-20 12:13:18 -0400

    ParentPotentialPayer table

--- db/migrate/20100919173253_parent_potential_payer.rb
+++ db/migrate/20100919173253_parent_potential_payer.rb
@@ -2,6 +2,7 @@ class ParentPotentialPayer < ActiveRecord::Migration
   def self.up
     create_table "person_parent_potential_payer", :force => true do |t|
       t.integer "entity_id"
+      t.integer "prospect_id"
       t.string "source"
       t.integer "government_school_entity_id"
       t.integer "government_school_district_entity_id"
