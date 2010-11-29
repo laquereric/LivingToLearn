e6742e6a6214a7c806591cb71c0ad0d3408b665a
ez <eric@Eric-Laquers-Computer.local>
2010-11-28 22:42:18 -0500

    Wrap for Watir

---
+++ app/models/browser.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+class Browser
+  cattr_accessor :browser
+
+  def self.get_browser
+    return self.browser if !self.browser.nil?
+    process_info_array= firefox_process= %x(ps auxwww | awk /firefox-bin/).split
+    if process_info_array.length>30 then
+      firefox_process= process_info_array[1]
+      %x( kill #{firefox_process} )
+    end
+    system "sh #{RAILS_ROOT}/lib/start_firefox_jssh.exe"
+    browser = FireWatir::Firefox.new
+    return browser
+  end
+
+end
