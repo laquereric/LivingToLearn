58d4117a47a8e53b73e3db5c37c6358912dadbc5
ez <eric@Eric-Laquers-Computer.local>
2010-11-29 22:48:37 -0500

    PDF added burst but lost database connection

--- app/models/document/pdf.rb
+++ app/models/document/pdf.rb
@@ -1,19 +1,57 @@
 require 'rubygems'
 require 'pdf/reader'
-class Document::Pdf <  Document
-  set_table_name :document_pdfs
+
+class Document::Pdf #< Document
+  #set_table_name :document_pdfs
+
+  attr_accessor :filename
   attr_accessor :pages
+  attr_accessor :doc
+
+  def self.tmp_dir
+    return File.join(RAILS_ROOT,'tmp','pdf_manipulate')
+  end
+
+  def self.init_tmp_dir()
+    %x[rm -r #{tmp_dir}] if File.exists?(tmp_dir)
+    %x[mkdir #{tmp_dir}]
+  end
 
   def page_count(arg)
-    @pages = arg
+    self.pages = arg
   end
 
   def self.parse(fn)
-p "parsing #{fn}"
     receiver= self.new
-    fn.gsub!(/~/,'/Users/eric')
     receiver.filename= fn
-    pdf = PDF::Reader.file(fn, receiver, :pages => false)
-    puts " #{receiver.pages} pages"
+    receiver.doc = PDF::Reader.file(fn, receiver, :pages => false)
+    return receiver
   end
+
+  def self.length(fn)
+    receiver= self.parse(fn)
+    return receiver.pages
+  end
+
+  def self.burst(fn)
+    return nil if !File.exists?(fn)
+    local_path= fn.split('/')[-1]
+    base_path= local_path.split('.')[0]
+    sys_path=  fn.split('/')[0..-2].join('/')
+    self.init_tmp_dir()
+    %x(cd #{self.tmp_dir}; cp #{fn} . )
+    %x(cd #{self.tmp_dir}; pdftk ./#{local_path} burst )
+    pages= Dir.glob( File.join(self.tmp_dir, "pg*.pdf") )
+    pages.each{ |pg|
+       pgn= pg.split('/')[-1].split('.')[0]
+       pg_fn= File.join(sys_path,"#{base_path}.#{pgn}.pdf")
+       %x(cp #{pg} #{pg_fn} )
+       p "#{pg_fn} created"
+    }
+    nfm= File.join(sys_path,"#{base_path}.master.pdf")
+    p "new file name: #{nfm}"
+    %x(cp #{fn} #{nfm})
+    %x(rm #{fn})
+  end
+
 end
