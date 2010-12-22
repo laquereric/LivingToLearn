d2909067d4889076b61e295addd287e2095d8e0d
ez <eric@Eric-Laquers-Computer.local>
2010-12-22 10:52:11 -0500

    Current Clients Spreadsheet Tweaks

--- app/models/spreadsheet/current_clients.rb
+++ app/models/spreadsheet/current_clients.rb
@@ -1,4 +1,25 @@
 class Spreadsheet::CurrentClients < Spreadsheet::Spreadsheet
+  def self.headers
+
+    [
+      'Client Id',
+      'Select',
+      'To Do',
+      'Program S',
+      'Prefix','First Name','Middle Name','Last Name','Suffix',
+      'Email',
+      'Last Attended Date','Last Consumed Hour','Last Paid Hour',
+      'Give Invoice','Contract Hrs Per Week','Prev Contract End Hour',
+      'Contracted Hours','Last Contract Hour', 'Closed End',
+      'Direct','Prepaid','Active',
+      'AddressLine1','AddressLine2','City','State','Zip','Phone1','Phone2','Phone3',
+      'DOB','Grade',
+      'ParentXxPrefix','ParentXxFirstName','ParentXxMiddleName','ParentXxLastName','ParentXxSuffix',
+      'ParentXyPrefix','ParentXyFirstName','ParentXyMiddleName','ParentXyLastName','ParentXySuffix',
+      'School District', 'School',
+      'Origin', 'Representatives','Result','Status','Location'
+    ]
+  end
 
 ############
 #
@@ -36,26 +57,6 @@ class Spreadsheet::CurrentClients < Spreadsheet::Spreadsheet
     File.join( 'TutoringClub', 'Data', 'Clients', self.google_filename )
   end
 
-  def self.headers
-
-    [
-      'Client Id',
-      'Select',
-      'To Do',
-      'Program S',
-      'Prefix','First Name','Middle Name','Last Name','Suffix',
-      'Email',
-      'Last Consumed Hour','Last Paid Hour',
-      'Give Invoice','Contract Hrs Per Week','Prev Contract End Hour','Contracted Hours','Last Contract Hour', 'Closed End',
-      'Direct','Prepaid','Active',
-      'AddressLine1','AddressLine2','City','State','Zip','Phone1','Phone2','Phone3',
-      'DOB','Grade',
-      'ParentXxPrefix','ParentXxFirstName','ParentXxMiddleName','ParentXxLastName','ParentXxSuffix',
-      'ParentXyPrefix','ParentXyFirstName','ParentXyMiddleName','ParentXyLastName','ParentXySuffix',
-      'School District', 'School',
-      'Origin', 'Representatives','Result','Status','Location'
-    ]
-  end
 
 end
 
