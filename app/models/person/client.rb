0f80041c9e63b6eddcf67fcff2a48ef3d4310e5c
ez <eric@Eric-Laquers-Computer.local>
2010-12-26 09:44:48 -0500

    Added invoice csv line methods

--- app/models/person/client.rb
+++ app/models/person/client.rb
@@ -1,5 +1,11 @@
 class Person::Client #< Person::PersonDetail
 
+  def self.csv_line(client,indent=0)
+    l = ""
+    l<< "#{ client[:result] }"
+    return l
+  end
+
   def self.each_client (&block)
     Spreadsheet::CurrentClients.each_client{ |client|
       yield(client)
@@ -82,6 +88,74 @@ class Person::Client #< Person::PersonDetail
     return l
   end
 
+  def self.csv_keys
+    [:student_first_name,:student_last_name,:district_name,:district_code,:client_code,:period_month,:period_year,:invoice_date,:school,:district_city,:district_state,:district_zip,:fc_rate,:sc_rate,:fc_hours,:sc_hours,:fc_amount,:sc_amount,:amount,:per_pupil_amount,:hours_in_program]
+  end
+
+  def self.csv_line_header
+    self.csv_keys.map{ |ky| ky.to_s }.join(',')
+  end
+
+  def self.csv_line(hash)
+     self.csv_keys.map{ |ky| hash[ky].to_s }.join(',')
+     #hash.each_value.map{ |vl| vl.to_s }.join(',')
+  end
+
+  def self.csv_hash(client_hash)
+    r={}
+    r[:client_code]= "\'#{client_hash[:client_id].to_i}\'"
+    r[:student_first_name]= client_hash[:first_name]
+    r[:student_last_name]= client_hash[:last_name]
+
+    r[:school]=client_hash[:school]
+    sd = Government::SchoolDistrict.for_code_name( client_hash[:school_district] )
+    Government::SchoolDistrict.nj_cache()
+
+    sd_id = Government::SchoolDistrict.id_from_code_name( client_hash[:school_district] )
+    r[:district_code]=sd_id
+    sd_rec = Government::SchoolDistrict.nj_cache[ sd_id ]
+
+    r[:district_name] = sd.name
+    r[:district_city] = sd_rec[:city]
+    r[:district_state] = sd_rec[:state]
+    r[:district_zip] = sd_rec[:zip]
+
+    r[:invoice_date] =  Invoice.get[:invoice_date]
+    r[:period_month] = month = Invoice.get[:period_month]
+    r[:period_year] = Invoice.get[:period_year]
+
+    sd_ca = Contract::SchoolDistrict.get_for_sd(sd)
+    fc = sd_ca[0]
+    r[:per_pupil_amount]= fc[:per_pupil_amount]
+
+    fc_hrs_field_sym = "fc_hrs#{month}".to_sym
+    fc_hours = client_hash[fc_hrs_field_sym]
+    fc_hours ||= 0
+    r[:fc_hours] = fc_hours
+    r[:fc_rate] = fc_rate = fc[:rate]
+    r[:fc_amount] = fc_amount = fc_hours * fc_rate
+
+    if sd_ca.length == 1
+      r[:hours_in_program]= fc[:hours_in_program]
+      sc_amount= 0
+      r[:sc_hours] = sc_hours = 0
+    else
+      sc = sd_ca[1]
+      sc_hrs_field_sym = "SC Hrs #{month}".to_sym
+      sc_hours = client_hash[sc_hrs_field_sym]
+      sc_hours ||= 0
+      r[:sc_hours] = sc_hours
+      r[:sc_rate] = sc_rate = sc[:rate]
+      r[:sc_amount]= sc_amount = sc_hours * sc_rate
+    end
+
+    return nil if fc_hours == 0 and sc_hours == 0
+
+    r[:amount]= fc_amount + sc_amount
+p r
+    return r
+  end
+
   def self.by_school_report(results,&block)
         total_consumed_hours = 0
         results.each_key{ |sdn|
@@ -129,11 +203,21 @@ class Person::Client #< Person::PersonDetail
       yield ""
   end
 
-  def self.by_school_hash (&block)
+  def self.by_school_array (&block)
+    rs=[]
+    Person::Client.each_client{ |client|
+      next if block_given? and !yield(client)
+      rs<< client
+    }
+    return rs
+  end
+
+  def self.by_school_hash (client_array)
       results= {}
-      Person::Client.each_client{ |client|
+      #Person::Client.each_client{ |client|
+      client_array.each{ |client|
       #Spreadsheet::CurrentClients.each_client{ |client|
-        next if block_given? and !yield(client)
+        #next if block_given? and !yield(client)
         sd = Person::Client.school_district_sym(client)
         school = Person::Client.school_sym(client)
         result = Person::Client.clean_result(client)
