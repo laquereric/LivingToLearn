0b567eaf0b9f8a5b82b873c42299b053f8dc1ac4
ez <eric@Eric-Laquers-Computer.local>
2010-12-27 20:21:01 -0500

    Invoice Date functions added

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,9 @@
 class Invoice
   def self.get
     return {
-      :period_month=> 11,
-      :period_year=> 2010,
-      :invoice_date=>'12/27/2010'
+      :period_month => Date.today.month-1,
+      :period_year => ( Date.today-(1.month) ).year,
+      :invoice_date => Date.today
     }
   end
 end
