e120f3ff6c2df0f70a5ae6b7a3fc00dda7aa0263
ez <eric@Eric-Laquers-Computer.local>
2011-01-02 08:31:41 -0500

    Make CurrentClient Spreadsheet compatible with new object reflection requirement

--- app/models/spreadsheet/current_clients.rb
+++ app/models/spreadsheet/current_clients.rb
@@ -1,50 +1,11 @@
 class Spreadsheet::CurrentClients < Spreadsheet::Spreadsheet
-  def self.headers
 
-    [
-      'Client Id',
-      'Select',
-      'To Do',
-      'Program S',
-      'Prefix','First Name','Middle Name','Last Name','Suffix',
-      'Email',
-      'Last Attended Date','Last Consumed Hour','Last Paid Hour',
-      'Give Invoice','Contract Hrs Per Week','Prev Contract End Hour',
-      'Contracted Hours','Last Contract Hour', 'Closed End',
-      'Direct','Prepaid','Active',
-      'AddressLine1','AddressLine2','City','State','Zip','Phone1','Phone2','Phone3',
-      'DOB','Grade',
-      'ParentXxPrefix','ParentXxFirstName','ParentXxMiddleName','ParentXxLastName','ParentXxSuffix',
-      'ParentXyPrefix','ParentXyFirstName','ParentXyMiddleName','ParentXyLastName','ParentXySuffix',
-      'School District', 'School',
-      'Origin', 'Representatives','Result','Status','Location',
-      'First Contract',
-      'FC Hrs 9',
-      'FC Hrs 10',
-      'FC Hrs 11',
-      'FC Hrs 12',
-      'FC Hrs 1',
-      'FC Hrs 2',
-      'FC Hrs 3',
-      'FC Hrs 4',
-      'FC Hrs 5',
-      'FC Hrs 6',
-      'FC Hrs 7',
-      'FC Hrs 8',
-      'Second Contract',
-      'SC Hrs 9',
-      'SC Hrs 10',
-      'SC Hrs 11',
-      'SC Hrs 12',
-      'SC Hrs 1',
-      'SC Hrs 2',
-      'SC Hrs 3',
-      'SC Hrs 4',
-      'SC Hrs 5',
-      'SC Hrs 6',
-      'SC Hrs 7',
-      'SC Hrs 8'
-      ]
+#############
+# Reflection of Spreadsheet in Object
+#############
+
+  def self.connected_object
+    Person::Client
   end
 
 ############
