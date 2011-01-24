3a6be7911d112047fa1684fe7189794026c62de9
ez <eric@Eric-Laquers-Computer.local>
2011-01-23 20:10:55 -0500

    Start of Irregular Schedule Class

---
+++ app/models/appointment/irregular_schedule.rb
@@ -0,0 +1,26 @@
+class Appointment::IrregularSchedule
+=begin
+Jan 6
+Jan 11
+Jan 20
+Jan 25
+Feb 1
+Feb 10
+Feb 15
+Feb 24
+Mar 1
+Mar 10
+Mar 15
+Mar 24
+Mar 29
+April 5
+April 14
+April 19
+April 28
+May 3
+May 5
+May 12
+May 19
+May 24
+=end
+end
