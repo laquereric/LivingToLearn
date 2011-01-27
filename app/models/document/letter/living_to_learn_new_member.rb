85c85ea35cb003c4ea92024a8f2258d8a1541a8f
ez <eric@Eric-Laquers-Computer.local>
2011-01-27 12:01:27 -0500

    Clean up OBFs in Living To Learn Member Letters

--- app/models/document/letter/living_to_learn_new_member.rb
+++ app/models/document/letter/living_to_learn_new_member.rb
@@ -1,5 +1,40 @@
 class Document::Letter::LivingToLearnNewMember <  Document::Letter::TwoWindowTemplate
 
+  def self.name_block(pdf)
+
+    pdf.font_size = 8
+    pdf.text "Living To Learn"
+    pdf.text "283-B Egg Harbor Rd. #122"
+    pdf.text "Sewell, NJ 08080-3156"
+
+  end
+
+  def self.return_address_bounding_box( doc , &block )
+
+    logo_width = 1.5.in
+
+    logo_top_left = [ doc.bounds.left , doc.bounds.top - 0.125 ]
+    #logo_height = self.page[:header_height]-0.5.in
+    logo_height = 1.in
+
+    return_address_top_left = [ doc.bounds.left + logo_width - 0.25.in , doc.bounds.top ]
+
+    return_address_width = 3.5.in-logo_width
+    return_address_height = 1.25.in
+
+    logo = File.join( RAILS_ROOT, 'public' , 'images' , 'livingtolearn.jpg' )
+    doc.image logo, :at => logo_top_left , :width => logo_width * 0.9, :height => logo_height * 0.9
+
+    doc.bounding_box( return_address_top_left , :width => return_address_width , :height => return_address_height ) {
+      #doc.stroke_bounds
+      doc.move_down( 0.125.in )
+      doc.indent( 0.125.in ){
+        self.name_block( doc )
+      }
+    }
+
+  end
+
 #############
 # Directory
 #############
@@ -32,9 +67,11 @@ p "Creating PDF letter #{filename} for #{member}"
 
       template.address_bounding_box( pdf ){ |doc|
         doc.text "#{ member[:contact_first_name] } #{ member[:contact_last_name] }"
+        doc.text "#{ member[:organization_name] }"
         doc.text "#{ member[:contact_address_line_1]}"
         doc.text "#{ member[:contact_address_line_2]}"
-        doc.text "#{ member[:contact_city]}, #{ member[:contact_state]} #{ member[:contact_zip].to_s.gsub('_',' ') }"
+        zip = sprintf( '%05d',member[:contact_zip].to_i )
+        doc.text "#{ member[:contact_city]}, #{ member[:contact_state]} #{zip}"
       }
 
       template.header_bounding_box( pdf ){ |doc|
@@ -53,34 +90,83 @@ p "Creating PDF letter #{filename} for #{member}"
 
 def self.header_for(doc,member)
 
-  doc.fill_color "0055DD"
-  doc.font_size = 12
-  doc.text "Acknowledgement of Registration"
-  doc.move_down(0.5.in)
-  doc.text "Your registration as a Member of"
-  doc.text "Living To Learn was accepted"
+  return if member.organization_type.nil? or member.organization_type.length == 0
+  doc.bounding_box( [ doc.bounds.top_left[0]+0.125.in , doc.bounds.top_left[1]-0.125.in  ],
+  :width => doc.bounds.width-0.25.in, :height => doc.bounds.height-0.25.in ) {
+    doc.fill_color "0055DD"
+    doc.font_size = 10
+    doc.text "Your registration as a #{member.organization_type.titleize} member Living To Learn was accepted. Please consider:",:style=>:bold
+
+    obfs = member.get_objectives_benefits_features
+    return if obfs.nil?
+    obfs.each{ |obf|
+      self.obf_feature(doc,obf)
+    }
+  }
 
 end
 
-def self.obf(doc,obf)
-
-  doc.move_down(0.125.in)
-  doc.indent(0.125.in){
-    doc.move_down(0.125.in)
-    doc.text 'Objective',:style=>:bold
-    doc.text obf.objective
-  }
+def self.obf_feature(doc,obf)
 
   doc.indent(0.125.in){
     doc.move_down(0.125.in)
-    doc.text 'Benefit',:style=>:bold
-    doc.text obf.benefit
-  }
-
-  doc.indent(0.125.in){
-    doc.move_down(0.125.in)
-    doc.text 'Feature',:style=>:bold
     doc.text obf.feature
+  }
+
+end
+
+def self.obf(doc,obf,index)
+
+  doc.fill_color "0055DD"
+  obf_height = 1.125.in
+  obf_top = doc.bounds.top - 3.75.in - ( index * obf_height )
+
+  section_width = 2.in
+
+  objective_left = doc.bounds.left + ( (section_width+0.125) * 0 )
+  benefit_left = doc.bounds.left + ( (section_width+0.125) * 1 )
+  feature_left = doc.bounds.left + ( (section_width+0.125) * 2 )
+
+  doc.bounding_box( [ objective_left , obf_top ],
+    :width => section_width-0.125.in, :height => obf_height-0.125.in ) {
+    doc.stroke_bounds
+    doc.bounding_box( [ doc.bounds.top_left[0]+0.125.in , doc.bounds.top_left[1]-0.125.in  ],
+      :width => doc.bounds.width-0.25.in, :height => obf_height-0.25.in ) {
+      doc.font_size = 8
+      doc.text 'Objective',:style=>:bold
+      doc.indent(0.125.in){
+       doc.font_size = 5
+        doc.text obf.objective
+      }
+    }
+  }
+
+  doc.bounding_box( [ benefit_left , obf_top ],
+    :width => section_width-0.125.in, :height => obf_height-0.125.in ) {
+    doc.stroke_bounds
+    doc.bounding_box( [ doc.bounds.top_left[0]+0.125.in , doc.bounds.top_left[1]-0.125.in  ],
+      :width =>  doc.bounds.width-0.25.in, :height => obf_height-0.25.in ) {
+      doc.font_size = 8
+      doc.text 'Benefit',:style=>:bold
+      doc.indent(0.125.in){
+        doc.font_size = 6
+        doc.text obf.benefit
+      }
+    }
+  }
+
+  doc.bounding_box( [ feature_left , obf_top ],
+    :width => section_width-0.125.in, :height => obf_height-0.125.in ) {
+    doc.stroke_bounds
+    doc.bounding_box( [ doc.bounds.top_left[0]+0.125.in , doc.bounds.top_left[1]-0.125.in  ], 
+      :width => doc.bounds.width-0.25.in, :height => obf_height-0.25.in ) {
+      doc.font_size = 8
+      doc.text 'Feature',:style=>:bold
+      doc.indent(0.125.in){
+        doc.font_size = 6
+        doc.text obf.feature
+      }
+    }
   }
 
 end
@@ -88,29 +174,29 @@ end
 def self.obfs_for(doc,member)
   obfs = member.get_objectives_benefits_features
   return if obfs.nil?
+  index = 0
   obfs.each{ |obf|
-    self.obf(doc,obf)
+    self.obf(doc,obf,index)
+    index += 1
   }
 end
 
 def self.body_for(doc,member)
-  doc.font_size = 12
+  doc.font_size = 10
+  doc.fill_color "000000"
   doc.text <<-eos
 Dear  #{member.contact_first_name} #{member.contact_last_name}
 
 Thank you for the opportunity to present the the advantages of membership in “Living to Learn” to you. We would like “Living to Learn” to become an important part in the growth and health of our communities and are encouraged by your support.
 
-It is our sincere hope that you will be completely satisfied with the services that “Living to Learn” will provide for you, your employees and their family members.
+It is our sincere hope that you will be completely satisfied with the services that “Living to Learn” will provide for you, your family, your employees, their family members, and even your customers and their families.
 eos
- self.obfs_for(doc,member)
-
- #{member.contact_first_name} 
+ #{member.contact_first_name}
  doc.text <<-eos
 
 We are always interested in suggestions that would allow us to improve our organization and we encourage you to submit any thoughts or questions you might have.
 
 Thank you again,
-
 
 Eric Laquer, CEO
 Living To Learn
@@ -120,6 +206,8 @@ Sewell, NJ 08080-3156
 LaquerEric@gmail.com
 856 270 6810
 eos
+
+  self.obfs_for(doc,member)
 
 end
 
--- app/models/spreadsheet/living_to_learn/objectives_benefits_feature.rb
+++ app/models/spreadsheet/living_to_learn/objectives_benefits_feature.rb
@@ -1,4 +1,4 @@
 class Spreadsheet::LivingToLearn::ObjectivesBenefitsFeature < Spreadsheet::SpreadsheetTable
-set_table_name ('spreadsheet__living_to_learn__objectives_benefits_features')
+  set_table_name ('spreadsheet__living_to_learn__objectives_benefits_features')
 end
 
