2751e211c14b2495f20c5e1be3afe739d8d87d80
ez <eric@Eric-Laquers-Computer.local>
2011-01-31 09:41:48 -0500

    Pluralize Spreadsheet names

--- app/models/spreadsheet/spreadsheet_table.rb
+++ app/models/spreadsheet/spreadsheet_table.rb
@@ -1,7 +1,8 @@
 class Spreadsheet::SpreadsheetTable < ActiveRecord::Base
 
   def self.name_array
-    self.to_s.split('::')[1..-1]
+    na = self.to_s.split('::')[1..-1]
+    return [ na[0..-2] , na[-1].pluralize ].flatten
   end
 
   def self.get_records_from_google_spreadsheet()
