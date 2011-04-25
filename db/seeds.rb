cf15223d6dfc7802dfce21328cfbd5d821d23125
ez <eric@Eric-Laquers-Computer.local>
2011-04-25 15:43:03 -0400

    Baseline Devise works   - emails not sent   - user names not collected

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+
+puts 'SETTING UP EXAMPLE USERS'
+user1 = User.create! :name => 'First User', :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user1.name
+user2 = User.create! :name => 'Other User', :email => 'otheruser@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'nuther New user created: ' << user2.name
+
