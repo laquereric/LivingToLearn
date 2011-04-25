919d6163aea1e80672fee21da7ac7e4e60c30e80
ez <eric@Eric-Laquers-Computer.local>
2011-04-25 12:33:58 -0400

    Baseline Devise Authentication requirements

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,3 +1,4 @@
+ActionMailer::Base.default_url_options = { :host => 'LivingToLearn.com' }
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.sendmail_settings = {
