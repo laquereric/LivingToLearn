924ddb44c133e3d28bfe06c70f715a90bd663989
ez <eric@Eric-Laquers-Computer.local>
2011-04-27 21:58:30 -0400

    Create goto route to allow urls for specific topics

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,22 @@
 class WelcomeController < ApplicationController
   layout 'home'
+  before_filter :get_marketing_contexts
 
-  def index
+  def get_marketing_contexts
     if user_signed_in?
       @existing_marketing_context_types =
         current_user.marketing_context_types
       @possible_marketing_context_types =
         MarketingContextType.all_except(@existing_marketing_context_types)
     end
+  end
+
+  def goto
+    @goto_topic_symbol = params[:topic_symbol]
+    render "index"
+  end
+
+  def index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ LivingToLearn::Application.routes.draw do
   resources :marketing_context_types
 
   match 'user/:user_id/add_marketing_context/:marketing_context_id', :to => 'users#add_marketing_context', :as => 'add_user_marketing_context'
-
   match 'user/:user_id/delete_marketing_context/:marketing_context_id', :to => 'users#delete_marketing_context', :as => 'delete_user_marketing_context'
+  match 'user/:user_id/select_marketing_context/:marketing_context_id', :to => 'users#select_marketing_context', :as => 'select_user_marketing_context'
+
+  match 'goto/:topic_symbol', :to => 'welcome#goto', :as => 'goto_topic'
 
   root :to => "welcome#index"
 
