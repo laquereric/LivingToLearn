371ca1a94d42133d1a4e4537b3ba2e7001a9e5df
ez <eric@Eric-Laquers-Computer.local>
2011-05-11 13:13:14 -0400

    Curriculum Base allows polymorphic lookups of curriculum items at various levels of hierarchy

---
+++ app/models/curriculum/base.rb
@@ -0,0 +1,22 @@
+class Curriculum::Base
+
+  def self.get_cpi ( ref )
+    r = {}
+    r[:klass_name] = klass_name = "Curriculum::CumulativeProgressIndicator"
+    r[:cpi_ref] = ref
+    r[:cpi] = cpi = klass_name.constantize.find_by_full_code(ref)
+    r[:cpi_description] = cpi.description
+    return r
+  end
+
+  def self.get_entry( ref_type , ref )
+    r = nil
+    r = get_cpi( ref ) if ref_type == "CPI"
+    return r
+  end
+
+  def self.get_test_entry
+    cpi = get_entry("CPI","NjS21clc 9.1.4.A.1")
+  end
+
+end
