61551426d387eb11ad2c29eb81198dde27733bf0
ez <eric@Eric-Laquers-Computer.local>
2011-05-25 10:11:24 -0400

    Added Potential KVN Sponsors

---
+++ app/models/potential_kvn_sponsors.rb
@@ -0,0 +1,50 @@
+class PotentialKvnSponsors < ActiveRecord::Base
+
+  require 'csv'
+  cattr_accessor :raw_csv_headers
+  cattr_accessor :field_csv_headers
+
+#########################
+# CSV Import methods
+##########################
+
+  def self.filename
+    File.join(Rails.root,'data','potential_kvn_sponsors.csv')
+  end
+
+  def self.row2hash(row)
+    hash = {}
+    self.field_csv_headers.each_index{ |i|
+      hash[ field_csv_headers[i] ] = row[i]
+    }
+    return hash
+  end
+
+  def self.get_records_from_csv
+    line_number = 0
+    rs = []
+    CSV::Reader.parse(File.open(self.filename, 'rb')) do |row|
+      if line_number == 0
+        self.raw_csv_headers = row
+        self.field_csv_headers = row.map{ |n| n.gsub('/','').gsub(' ','').underscore }
+      else
+        rs << self.create( row2hash(row) )
+      end
+      line_number +=1
+    end
+    return rs
+  end
+
+#########################
+# utility
+##########################
+
+  def zip_code_5
+    return zip_code.split('-')[0].to_i
+  end
+
+  def self.uniq_zip_codes
+    PotentialKvnSponsors.all.map{ |s| s.zip_code_5 }.uniq
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110524180332) do
+ActiveRecord::Schema.define(:version => 20110525134931) do
 
   create_table "curriculum_content_areas", :force => true do |t|
     t.string   "code"
@@ -250,6 +250,65 @@ ActiveRecord::Schema.define(:version => 20110524180332) do
   create_table "person_school_district_administrator", :force => true do |t|
     t.integer "entity_id"
     t.integer "government_school_district_detail_id"
+  end
+
+  create_table "potential_kvn_sponsors", :force => true do |t|
+    t.string   "yp_spend"
+    t.string   "year1rst_appeared"
+    t.string   "contact_gender"
+    t.string   "location_delivery_point_bar_code"
+    t.string   "location_address_city"
+    t.string   "city"
+    t.string   "address"
+    t.string   "category"
+    t.string   "franchise_specialty1"
+    t.string   "location_address_zip"
+    t.string   "franchise_specialty2"
+    t.string   "credit_alpha_score"
+    t.string   "sales_volume_range"
+    t.string   "location_carrier_route"
+    t.string   "mailing_delivery_point_bar_code"
+    t.string   "zip_code"
+    t.string   "company_name"
+    t.string   "notes"
+    t.string   "primary_sic"
+    t.string   "actual_employee_size"
+    t.string   "location_address"
+    t.string   "adsize_in_yellow_pages"
+    t.string   "industry_specific_codes"
+    t.string   "square_footage"
+    t.string   "office_size"
+    t.string   "credit_numeric_score"
+    t.string   "primary_sic_description"
+    t.string   "pc_code"
+    t.string   "contact_title"
+    t.string   "county"
+    t.string   "mailing_carrier_route"
+    t.string   "firm_individual"
+    t.string   "headquarters_branch"
+    t.string   "secondary_sic1"
+    t.string   "contact_prof_title"
+    t.string   "phone_number"
+    t.string   "infousa_id"
+    t.string   "secondary_sic2"
+    t.string   "secondary_sic_description1"
+    t.string   "employee_size_range"
+    t.string   "last_name"
+    t.string   "secondary_sic_description2"
+    t.string   "actual_sales_volume"
+    t.string   "location_address_state"
+    t.string   "record_obsolescence_date"
+    t.string   "web_address"
+    t.string   "source"
+    t.string   "date_list_produced"
+    t.string   "metro_area"
+    t.string   "public_private_flag"
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "state"
+    t.string   "benefactor_potential"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "services", :force => true do |t|
