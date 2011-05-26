58435f7961cb3cf4785bdb183b7f13ef5b0b2bc9
ez <eric@Eric-Laquers-Computer.local>
2011-05-26 16:05:12 -0400

    Resources Protected Links appear as available Must 'backdoor' set user admin boolean to true to upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110526021100) do
+ActiveRecord::Schema.define(:version => 20110526194710) do
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                                    :default => "", :null => false
+    t.string   "encrypted_password",        :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                            :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "current_marketing_context"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
 
   create_table "curriculum_content_areas", :force => true do |t|
     t.string   "code"
@@ -464,6 +485,7 @@ ActiveRecord::Schema.define(:version => 20110526021100) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
