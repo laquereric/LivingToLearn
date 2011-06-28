cf88d64517ca4b88b8a57a86d91d7b28b66a1e3d
ez <eric@Eric-Laquers-Computer.local>
2011-06-28 07:29:06 -0400

    Breadcrumb cleanup, eliminated dead links from terminal nodes and refactored controller

--- app/controllers/curriculum_controller.rb
+++ app/controllers/curriculum_controller.rb
@@ -6,6 +6,23 @@ class CurriculumController < ApplicationController
     render :action => :index
   end
 
+  def complete(h)
+    h[:terminal]= h[:node].children.length == 0 ? true : false
+
+    #TODO why not? h[:target] ||= h[:node].target
+
+    klass = h[:node].target_node_klass_name.constantize
+    h[:target] = klass.find(h[:node].target_node_object_id)
+
+    h[:name] = h[:target].name
+    h[:name] ||= h[:target].description
+    h[:name] ||= h[:target].code
+
+    h[:name] = 'Curricula' if  h[:name] == 'Root'
+
+    return h
+  end
+
   def index
 
     @center = if params[:name] == 'root'
@@ -14,31 +31,16 @@ class CurriculumController < ApplicationController
     elsif params[:node_id] == 'root'
       curriculum_klass = "Curriculum::#{params[:name]}".constantize
       node = curriculum_klass.root_node
-      { :node => node, :target => node.target }
+      { :node => node }
     else
       node = CurriculumItem.find_by_id(params[:node_id])
-      { :node => node, :target => node.target }
+      { :node => node }
     end
 
-    @center[:name] = @center[:target].name
-    @center[:name] ||= @center[:target].description
-    @center[:name] ||= @center[:target].code
+    self.complete(@center)
 
-    @parent = if (@center[:node].is_root?)
-      nil
-    else
-      node = @center[:node].parent
-      #target = node.target
-      klass= node.target_node_klass_name.constantize
-      target = klass.find(node.target_node_object_id)
-      name   = target.name
-      name ||= target.description
-      name ||= target.code
-      {:node => node, :target => target, :name => name }
-    end
-
-    @children = @center[:node].children.map{ |n|
-      { :node => n, :target => n.target }
+    @children = @center[:node].children.map{ |node|
+      self.complete( { :node => node } )
     }.select{ |t|
       t[:target].has_deadlines?
     }.sort{ |x,y|
@@ -48,6 +50,18 @@ class CurriculumController < ApplicationController
       h[:name] ||= h[:target].description
       h[:name] ||= h[:target].code
     }
+
+   @parent= complete ( {:node => @center[:node].parent } )  if !(@center[:node].is_root?)
+   @grand_parent= complete ( {:node => @parent[:node].parent } ) if @parent and !(@parent[:node].is_root?)
+
+
+    @great_grand_parent = if @grand_parent and !(@grand_parent[:node].is_root?)
+      complete ( {:node => @grand_parent[:node].parent } )
+    end
+
+    @great_great_grand_parent = if @great_grand_parent and !(@great_grand_parent[:node].is_root?)
+      complete ( {:node => @great_grand_parent[:node].parent } )
+    end
 
     @all_grades = Curriculum::Grade.all
 
