7c1fc5c40e6c464bc14bf4ab1c0f3a916086d015
ez <eric@Eric-Laquers-Computer.local>
2011-07-16 09:41:46 -0400

    Fix iphone sign-in try

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < Devise::SessionsController
         format.html { render :template=>'users/sessions/new'}
         format.iphone { render :template=>'users/sessions/new'}
       end
-
-
+  end
+  def after_sign_in_path_for(resource_or_scope)
+     '/'
   end
 end
 
