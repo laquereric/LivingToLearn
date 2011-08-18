7dd4825054f2418a52729d33c836f6278a4e517b
ez <eric@Eric-Laquers-Computer.local>
2011-08-18 14:43:24 -0400

    Added binding to ERB processing of tabs and cards then implemented a couple of examples

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,24 +1,40 @@
+require 'erb'
+
 class Content
+  cattr_accessor :view_binding
+  cattr_accessor :target
+
+  def self.template_result( t )
+    template = ERB.new( t )
+    return template.result self.view_binding
+  end
+
+  def self.get_template( filename )
+    return File.new( filename, 'r' ).read
+  end
+
   def self.get_html( target, config )
+    self.target =  target
     class_name = if target.class == Class
       target.to_s
     else
       target.class.to_s
     end
-    p "class_name #{class_name}"
+p "class_name #{class_name}"
     class_name_parsed = class_name.split('::').map { |n| n.downcase.to_sym }
-    p "class_name_parsed #{class_name_parsed.inspect}"
+p "class_name_parsed #{class_name_parsed.inspect}"
 
     if class_name_parsed[1] == :tab
       if config[:tab].nil?
         config[:tab] = class_name_parsed[2]
       end
     end
- 
+
     if class_name_parsed[1] == :card
       config[:tab] = class_name_parsed[3]  if config[:tab].nil?
       config[:card] = class_name_parsed[4]  if config[:card].nil?
     end
+
 p "config #{config.inspect}"
 
     filename =  if config[:filename]
@@ -28,7 +44,12 @@ p "config #{config.inspect}"
     elsif config[:access] and config[:tab]
       File.join(Rails.root,'app','views','touch',config[:access].to_s,"tab_#{config[:tab].to_s}.html.erb")
     end
-    return File.new( filename, 'r' ).read
+
+    template = self.get_template( filename )
+    result = self.template_result( template )
+
+    return result
   end
+
 end
 
