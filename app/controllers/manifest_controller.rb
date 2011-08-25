0e53642264d4b59d7777b5a0fa61be72503a9ff8
ez <eric@Eric-Laquers-Computer.local>
2011-08-25 17:24:56 -0400

    Just Application Cache test works

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -1,12 +1,17 @@
 class ManifestController  < ApplicationController
+
   def manifest_content
-manifest_content = <<-MANIFESTCONTENT
+    r = <<-MANIFESTCONTENT
 CACHE MANIFEST
 
+http://lvh.me:3001/404.html
+
 MANIFESTCONTENT
+    return r
   end
 
   def manifest
+    response.headers["Content-Type"]= "text/cache-manifest"
     render :inline => self.manifest_content
   end
 
